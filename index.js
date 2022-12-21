e91b8ee7ed7e832133d2196fcd2433d58f554953
LuizEFV <119553902+LuizEFV@users.noreply.github.com>
2022-12-20 21:12:25 -0300

    Listagem de endpoints e criação de enpoints de leitura, tanto quanto a individual quanto a de todos os itens.

--- index.js
+++ index.js
@@ -8,6 +8,34 @@ app.get('/hello', (req, res) => {
   res.send('Hello World');
 })
 
+/*
+Lista de endpoints na aplicação
+CRUD: Create, Read (Single & All), Update and Delete
+- [GET] /mensagens - Retorna a lista de mensagens
+- [POST] /mensagens - Cria uma nova mensagem
+- [PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
+- [DELETE] /mensagens/{id} - Remover uma mensagem pelo ID 
+*/
+
+const mensagens = [
+    "Essa é a primeira mensagem",
+    "Essa é a segunda mensagem"
+];
+
+/* - [GET] /mensagens - Retorna a lista de mensagens */
+
+app.get('/mensagens', (req, res) => {
+    res.send(mensagens);
+})
+
+/* - [GET] /mensagens - Retorna a lista de mensagens */
+
+app.get('/mensagens/:id', (req, res) => {
+        const id = req.params.id - 1;
+        const mensagem = mensagens[id];
+        res.send(mensagem);
+});
+
 app.listen(port, () => {
     console.info(`App rodando em http://localhost:${port}` );
 });
